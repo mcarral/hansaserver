dbb466fee3737a55d7855ab2e876825b4a43f07d
Matias Carral <matiascarral@gmail.com>
2016-05-09 01:04:59 -0400

    Installer support multiple services name

--- instsvhansaserver.sh
+++ instsvhansaserver.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 urlscript=/etc/init.d
+servicename="hansaserver"
 
 echo_success(){
   printf "$(tput setaf 2)%155s$(tput sgr0)\n" "[OK]"
 }
+
 echo_failure(){
   printf "$(tput setaf 1)%155s$(tput sgr0)\n" "[FAILED]"
 }
+
 countspace(){
   count=0
   export IFS=" "
@@ -17,70 +20,120 @@ countspace(){
   echo "$count"
 }
 
-if [ -f $urlscript/hansaserver ]; then
-  rm -rf $urlscript/hansaserver	
-fi
-wget -P$urlscript/ https://raw.github.com/matiascarral/hansaserver/master/hansaserver
-echo $"[...] Waiting \n"
-sleep 10
-if [ -f $urlscript/hansaserver ]; then
-  echo -n $"Ingrese la ruta completa donde se encuentra la aplicacion HANSA:\n"
-  echo -n $"Ej: /home/HANSA \n"
+getServiceName(){
+  echo -e $"Ingrese un nombre para el servicio HANSA:"
+  echo -e $"Ej: hansaempresa, hansaserver, hansamiempresa, etc"
+  
+  read servicename
+  compressed="$(echo $servicename | sed -e 's/[^[:alnum:]\-]//g')"
+  
+  while ([ -z "${servicename}" ] || [ "$compressed" != "$servicename" ]); do
+    echo -e $"El nombre del servicio es requerido y alphanumerico:"
+    
+    read servicename
+    compressed="$(echo $servicename | sed -e 's/[^[:alnum:]\-]//g')"
+  done
+  servicenametmp="${servicename}2"
+}
+
+getUrlHansa(){
+  echo -e $"Ingrese la ruta completa donde se encuentra la aplicacion HANSA:"
+  echo -e $"Ej: /home/HANSA"
   read urlhansa
   count=$(countspace)
-  while [ ! "$count" -eq "1" ]; do
-    echo -n $"La ruta no puede contener espacios. Ingrese nuevamente:\n"
+  while [ ! "${count}" -eq "1" ]; do
+    echo -e $"La ruta no puede contener espacios. Ingrese nuevamente:"
     read urlhansa
     count=$(countspace)
   done
-  while [ ! -d "$urlhansa" ]; do
-    echo -n $"El directorio ingresado no existe. Ingrese nuevamente:\n"
+  while [ ! -d "${urlhansa}" ]; do
+    echo -e $"El directorio ingresado no existe. Ingrese nuevamente:"
     read urlhansa
   done
-  while [ ! -f "$urlhansa"/hansa-server ]; do
-    echo -n $"No se encuentra el ejecutable hansa-server en $urlhansa/hansa-server \n"
+  while [ ! -f "${urlhansa}/hansa-server" ]; do
+    echo -e $"No se encuentra el ejecutable hansa-server en $urlhansa/hansa-server"
   done
-  namereplace="urlhansaprogram"
-  sed -e 's@'"$namereplace"'@'"$urlhansa"'@g' $urlscript/hansaserver > $urlscript/hansaserver2
-  mv -f $urlscript/hansaserver2 $urlscript/hansaserver
-  echo -n $"Ingrese un numero de puerto: \n"
+}
+
+getPortHansa(){
+  echo -e $"Ingrese un numero de puerto:"
   while read porthansa
   do
     NUM=$(( ${porthansa#0} ))
-    if [ "$NUM" -lt 1000 ] || [ "$NUM" -gt 3000 ]; then
+    if [ "${NUM}" -lt 1000 ] || [ "${NUM}" -gt 3000 ]; then
       echo $"Puerto incorrecto, desde el 1000 al 3000:"
     else
       break
     fi
   done
+}
+
+# Get service name
+getServiceName
+
+# Remove service if exists
+servicefile="${urlscript}/${servicename}"
+if [ -f "$servicefile" ]; then
+  rm -rf "$servicefile"	
+fi
+
+# Download service template
+touch "${urlscript}/${servicename}"
+wget https://raw.github.com/matiascarral/hansaserver/master/hansaserver -O "${urlscript}/${servicename}" 
+echo -e $"[...] Waiting"
+sleep 10
+
+# Verify if template download success
+if [ -f "${urlscript}/${servicename}" ]; then  
+  
+  # Replace url hansa in service template
+  namereplace="hansaserver"
+  sed -e "s@${namereplace}@${servicename}@g" "${urlscript}/${servicename}" > "${urlscript}/${servicenametmp}"
+  mv -f "${urlscript}/${servicenametmp}" "${urlscript}/${servicename}"
+
+  # Get Url hansa application
+  getUrlHansa
+
+  # Replace url hansa in service template
+  namereplace="urlhansaprogram"
+  sed -e "s@${namereplace}@${urlhansa}@g" "${urlscript}/${servicename}" > "${urlscript}/${servicenametmp}"
+  mv -f "${urlscript}/${servicenametmp}" "${urlscript}/${servicename}"
+  
+  # Get hansa port number
+  getPortHansa
+  
+  # Replace hansa port number in service template
   porthansa=$NUM
   namereplace="porthansaprogram"
-  sed -e 's@'$namereplace'@'$porthansa'@g' $urlscript/hansaserver > $urlscript/hansaserver2
-  mv -f $urlscript/hansaserver2 $urlscript/hansaserver
-  chmod ug+x $urlscript/hansaserver
-  if [ ! -f /etc/rc0.d/K09hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc0.d/K09hansaserver
+  sed -e "s@${namereplace}@${porthansa}@g" "${urlscript}/${servicename}" > "${urlscript}/${servicenametmp}"
+  mv -f "${urlscript}/${servicenametmp}" "${urlscript}/${servicename}"
+  
+  # Config runlevel
+  chmod ug+x "${urlscript}/${servicename}"
+  if [ ! -f "/etc/rc0.d/K09${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc0.d/K09${servicename}"
   fi
-  if [ ! -f /etc/rc1.d/K09hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc1.d/K09hansaserver
+  if [ ! -f "/etc/rc1.d/K09${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc1.d/K09${servicename}"
   fi
-  if [ ! -f /etc/rc2.d/S91hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc2.d/S91hansaserver
+  if [ ! -f "/etc/rc2.d/S91${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc2.d/S91${servicename}"
   fi
-  if [ ! -f /etc/rc3.d/S91hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc3.d/S91hansaserver
+  if [ ! -f "/etc/rc3.d/S91${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc3.d/S91${servicename}"
   fi  
-  if [ ! -f /etc/rc4.d/S91hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc4.d/S91hansaserver
+  if [ ! -f "/etc/rc4.d/S91${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc4.d/S91${servicename}"
   fi
-  if [ ! -f /etc/rc5.d/S91hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc5.d/S91hansaserver
+  if [ ! -f "/etc/rc5.d/S91${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc5.d/S91${servicename}"
   fi
-  if [ ! -f /etc/rc6.d/K09hansaserver ]; then
-    ln -s $urlscript/hansaserver /etc/rc6.d/K09hansaserver
+  if [ ! -f "/etc/rc6.d/K09${servicename}" ]; then
+    ln -s "${urlscript}/${servicename}" "/etc/rc6.d/K09${servicename}"
   fi
-  service hansaserver start
+  service "${servicename}" start
 else
+  
   echo $"Error al descargar hansaserver, verifique la conexion a internet. \n"
   echo_failure
 fi
